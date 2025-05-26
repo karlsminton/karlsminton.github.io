aa26d3f425b92eeaaa6fcce6cf33ac0b7059df9a
Karl Minton <kminton@frontier.co.uk>
2025-05-26 11:50:17 +0100

    add additional features

--- template.php
+++ template.php
@@ -9,16 +9,37 @@
         <style>
             body {
                 font-family: Calibri, Arial;
+                margin: 0;
             }
             .container {
                 max-width: 1200px;
                 margin: auto;
             }
+            main {
+                display: flex;
+            }
+            aside {
+                min-width: 300px;
+                padding: 15px;
+            }
+            header {
+                background: rgb(0, 0, 127.5);
+                margin-bottom: 15px;
+                color: #fff;
+            }
+            .me > a {
+                color: #fff;
+                text-decoration: none;
+            }
         </style>
     </head>
     <body>
-        <header></header>
-        <main>
+        <header>
+            <div class="container">
+                <!-- Come up with a heading and link to linkedin / github -->
+            </div>
+        </header>
+        <main class="container">
             <aside>
                 <h3>All Articles</h3>
                 <ul>
@@ -27,7 +48,7 @@
                     <?php endforeach; ?>
                 </ul>
             </aside>
-            <section class="container">
+            <section>
                 <!-- BODY -->
                  <?= $contents ?>
                 <!-- BODY END -->
